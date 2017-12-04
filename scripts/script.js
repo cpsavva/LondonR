39b4cb59842028ef0943a002b997192430451a81
catherine savva <csoriasavva@gmail.com>
2017-12-04 15:13:55 -0500

    created modals for the carousel pics

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,43 @@
-
 $(document).ready(function() {  
-// Carousel
-   $("#carousel-example-generic").swiperight(function() {  
-      $("#carousel-example-generic").carousel('prev');  
+
+var button = $(event.relatedTarget) 
+var recipient = button.data('whatever') 
+var modal = $(this)
+
+
+// Carousel Media Query
+   $("#services-carousel").swiperight(function() {  
+      $("#services-carousel").carousel('prev');  
     });  
-   $("#carousel-example-generic").swipeleft(function() {  
-      $("#carousel-example-generic").carousel('next');  
+   $("#services-carousel").swipeleft(function() {  
+      $("#services-carousel").carousel('next');  
    });  
 
+// Carousel Modal
+	$('#carModal').on('show.bs.modal', function(event){
+			var button = $(event.relatedTarget) 
+			var recipient = button.data('whatever') 
+			var carouselImage = "<img src='../css/images/" + recipient + ".jpg'/>";
+			console.log(recipient)
+
+
+			$("#car-content").empty()
+			// $("#services-carousel").clone().appendTo($('#car-content'));
+			$("#car-content").append(carouselImage)
+		});
+
+
 //eshop modals
-		$('#eshopModal').on('show.bs.modal', function (event) {
+	$('#eshopModal').on('show.bs.modal', function (event) {
 		var button = $(event.relatedTarget) 
 		var recipient = button.data('whatever') 
-		console.log("idk why this isn't working")
-
-		var eshopimg = "<img src='./css/images/" + recipient + ".jpeg'/>";
-		console.log(recipient)
-		
 		var modal = $(this)
+		// console.log("idk why this isn't working")
+		var eshopimg = "<img src='./css/images/" + recipient + ".jpeg'/>";
+		// console.log(recipient)
 		$(".modal-content").empty();
-		modal.find('.modal-content').append('<img src="./css/images/'+ recipient +'.jpeg">');
+		modal.find('.modal-content').append(eshopimg);
 	}) 
-
 
 
 
